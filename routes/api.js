cd6e7bf7fd292505f169537b31ff120276c76e65
Perry Donham <perryd@bu.edu>
2017-06-10 15:48:26 -0400

    add helper function to see if username is in db

--- routes/api.js
+++ routes/api.js
@@ -1,62 +1,102 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cs411');
-var db = mongoose.connection;
-var Schema = mongoose.Schema;
-var personSchema = new Schema ({
-  name: String,
-  UID: String,
-  department: String
-});
-var people = mongoose.model('people', personSchema);
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/cs411')
+const db = mongoose.connection
+const Schema = mongoose.Schema
+const personSchema = new Schema({
+    name      : String,
+    UID       : String,
+    department: String
+})
+const people = mongoose.model('people', personSchema)
 
 // POST Create a new user
-router.post('/db', function(req, res, next) {
-  aPerson = new people(
-      req.body
-  );
-  aPerson.save(function(err) {
-    if (err) {res.send(err);}
+router.post('/db', function (req, res, next) {
+    aPerson = new people(
+        req.body
+    )
+    aPerson.save(function (err) {
+        if (err) {
+            res.send(err)
+        }
         //send back the new person
-    else {res.send (aPerson)}
-  })
-});
+        else {
+            res.send(aPerson)
+        }
+    })
+})
 
 //GET Fetch all users
 router.get('/db', function (req, res, next) {
-  people.find({}, function (err, results) {
-    res.json(results);
-  })
+    people.find({}, function (err, results) {
+        res.json(results)
+    })
 
-});
+})
 
-//GET Fetch single user, match /users/db/Frank
-router.get('/db/:_id', function (req, res, next) {
-  people.find({_id: req.param('_id')}, function (err, results) {
-    res.json(results);
-  });
-});
+/*
+ //GET Fetch single user, match /users/db/Frank
+ router.get('/db/:_id', function (req, res, next) {
+ people.find({_id: req.param('_id')}, function (err, results) {
+ res.json(results);
+ });
+ });
+ */
 
+router.get('/db/:name', function (req, res, next) {
+    findByName(req.params.name)
+        .then(function (status) {
+            res.json(status)
+        })
+        .catch(function (status) {
+            res.json(status)
 
+        })
+})
 
 //PUT Update the specified user's name
 router.put('/db/:_id', function (req, res, next) {
-  people.findByIdAndUpdate(req.params._id, req.body, {'upsert': 'true'}, function (err, result) {
-    if (err) {res.json({message: 'Error updating'});}
-    else {console.log('updated'); res.json({message: 'success'})};
-  })
+    people.findByIdAndUpdate(req.params._id, req.body, {'upsert': 'true'}, function (err, result) {
+        if (err) {
+            res.json({message: 'Error updating'})
+        }
+        else {
+            console.log('updated')
+            res.json({message: 'success'})
+        }
 
-});
+    })
+
+})
 
 
 //DELETE Delete the specified user
 router.delete('/db/:_id', function (req, res, next) {
-  people.findByIdAndRemove(req.params._id, function (err, result) {
-    if(err) {res.json({message: 'Error deleting'});}
-    else {res.json({message: 'success'});}
-  })
-});
+    people.findByIdAndRemove(req.params._id, function (err, result) {
+        if (err) {
+            res.json({message: 'Error deleting'})
+        }
+        else {
+            res.json({message: 'success'})
+        }
+    })
+})
 
-module.exports = router;
+
+let findByName = function (checkName) {
+    return new Promise(function (resolve, reject) {
+        people.find({name: checkName}, function (err, results) {
+            console.log(results, results.length)
+            if (results.length > 0) {
+                resolve({found: results})
+            }
+            else {
+                reject({found: false})
+            }
+//    return ( (results.length  > 0) ? results : false)
+        })
+    })
+}
+module.exports = router
